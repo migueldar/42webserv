26ed261ca60d1306cf130a3a3f94cf23734b9cfc
KiriamBlu <Kiriamblu@gmail.com>
2024-01-31 18:12:12 +0100

    Some definition classes

--- inc/parser.hpp
+++ inc/parser.hpp
@@ -4,37 +4,58 @@
 #include <iostream>
 #include <vector>
 
+#define METHODS_NUM 3
+
+enum methodsEnum{
+    GET = 0;
+    POST;
+    DELETE;
+}
 
 class parserLocations{
-    //
-    std::string locationPath = "";
-
-    //std::string rootPath;
+    std::string  root;
 
     //NOT DEFAULT NEEDED
     std::string indexPath;
+
+    // NO DEFAULT NEEDED 
+    std::string redirectionUrl = "";
+
+    //MANAGES ESPECIFIC RULES FOR METHODS
+    uint8_t methods[METHODS_NUM];
+
+    //OFF predeterminado
+    bool autoindex;
+
 };
 
+
 class parserServer {
+
+    uint32_t maxBodySize;
 
     // FULL 4 bytes AUTOASIGNABLE
     uint32_t serverID; 
 
-    // RANGES [0, 65535]
-    uint16_t port;
-
     // MUST HAVE DEFAULT    
     std::string serverName;
 
-    // ALL PATHS EITHER EXISTS OR NOT
-    std::vector<parserLocations> routes;
+    // RANGES [0, 65535]
+    uint16_t port;
+
+    //Key:Status | Value:Route_to_html
+    std::map<uint16_t, std::string> parserErrPages;
+
+    // Key:LocationPath | Value:classLocation 
+    std::map<std::string, parserLocations> routes;
 
 };
 
 class parserFile{
+
     uint32_t numfile; 
 
-    std::vector<uint32_t> *prioIdServ;
+    std::vector<uint32_t> prioIdServ;
 
     std::vector<parserServer> serverDefinitinos;
 };
