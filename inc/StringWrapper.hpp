4b48fa4ee39a6a5a383444d37a1baba57a362017
migueldar <miguelarp3907@gmail.com>
2024-07-12 20:21:35 +0200

    done, needs testing

--- inc/StringWrapper.hpp
+++ inc/StringWrapper.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <deque>
+#include <vector>
 #include <iostream>
 
 #define MAX_STRING_SIZE 100000
@@ -12,16 +13,18 @@
 //that is all nodes but the last one must have size MAX_STRING_SIZE 
 class stringWrap {
 	private:
-		std::deque<std::string> deq;
+		std::deque<std::vector<char> > deq;
 	public:
 		stringWrap();
 		stringWrap(const stringWrap& other);
-		~stringWrap();
+		~stringWrap(); 
 		stringWrap& operator=(const stringWrap& other);
-		stringWrap&	operator+=(std::string other);
+		stringWrap& operator=(const std::string& other);
+		stringWrap&	operator+=(const std::string& data);
 		//cannot be a reference because if you add yourself it fails
 		stringWrap&	operator+=(const stringWrap other);
-		std::string	popFirst();
+		stringWrap& addData(const char* str, size_t size);
+		char*		popFirst(int& len);
 		bool		empty() const;
 		std::string	substr(size_t begin = 0, size_t len = std::string::npos) const;
 		stringWrap	subdeque(size_t begin = 0, size_t len = std::string::npos) const;
@@ -29,7 +32,7 @@ class stringWrap {
 		//if length of string to find is more than MAX_STRING_SIZE doesnt work
 		size_t		find(std::string toFind, size_t pos = 0) const;
 		char&		operator[](size_t pos);
-		const std::deque<std::string>& getDeque() const;
+		const std::deque<std::vector<char> >& getDeque() const;
 };
 
 std::ostream &operator<<(std::ostream &o, const stringWrap& prt);
